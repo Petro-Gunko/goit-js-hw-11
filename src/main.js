b23aa2931512e0bc4c7519769e703f081446cb16
petro-gunko <petryxa176@gmail.com>
2024-01-11 09:18:59 +0200

    homework 11.6

--- src/main.js
+++ src/main.js
@@ -3,29 +3,22 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const getImages = (query) => {
-    loaderRef.style.display = 'block';
-    return fetch(`https://pixabay.com/api/?key=41717891-03a798ff6847d2641a93432cc&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`)
-        .then((result) => {
-            if (!result.ok) {
-                throw new Error(result.status);
-            }
-            return result.json();
-        })
-        .finally(() => {
-            loaderRef.style.display = 'none';
-        });
-};
 
 const formRef = document.querySelector('.form');
 const galleryRef = document.querySelector('.gallery');
 const loaderRef = document.querySelector('.loader');
+
 loaderRef.style.display = 'none';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
+
 
 formRef.addEventListener('submit', (event) => {
     event.preventDefault();
     const searchQuery = event.currentTarget.elements.query.value.trim();
-
     getImages(searchQuery)
         .then((result) => {
             if (result.hits.length === 0) {
@@ -57,17 +50,29 @@ formRef.addEventListener('submit', (event) => {
             }).join("");
 
             galleryRef.insertAdjacentHTML(`beforeend`, markup);
-            new SimpleLightbox(`.gallery a`,
-            {
-                captionDelay: 250,
-                captionsData: 'alt',
-            });
+            lightbox.refresh();
         })
         .catch((error) => {
             iziToast.error({
                 position: 'topRight',
-                message: 'Sorry. Please try again!'
+                message: 'Sorry, there are no images matching your search query. Please try again!'
             });
         });
     
 });
+
+const getImages = (query) => {
+    loaderRef.style.display = 'block';
+    return fetch(`https://pixabay.com/api/?key=41717891-03a798ff6847d2641a93432cc&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`)
+        .then((result) => {
+            if (!result.ok) {
+                throw new Error(result.status);
+            }
+            return result.json();
+        })
+        .finally(() => {
+            loaderRef.style.display = 'none';
+        });
+};
+
+
